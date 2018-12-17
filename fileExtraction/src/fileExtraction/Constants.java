25ab938d86e0ccc93711c249201f3b39a7596897
Raj <rajanikanthg@gmail.com>
2018-12-17 15:18:37 +1100

    init

--- fileExtraction/src/fileExtraction/Constants.java
+++
@@ -1,9 +0,0 @@
-package fileExtraction;
-
-public final class Constants {
-
-	public static final String INPUT_DATA = "c:\\Sample.txt";
-	public static final String OUTPUT_CSV = "c://data.csv";
-	public static final String CHARSET_UFT8= "ISO-8859-1";
-	public static final String SPECIAL_CHAR= "[!@#$%&*,()_+=|<>?{}\\\\[\\\\]~-]";
-}
